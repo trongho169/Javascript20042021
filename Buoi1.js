2f528e9ae095a0bec8bf9008e63c9c38daacdd2b
HoVanTrong <vantrong169.ute@gmail.com>
2021-04-22 19:15:55 +0700

    khaibaobien

---
+++ Buoi1.js
@@ -0,0 +1,30 @@
+//1: Tạo thư mục quản lý source code .git (mỗi lần add new folde thì git init (chỉ khai báo một lần))
+// git init
+
+//2: Kiểm tra sự thay đổi của folder
+// git status
+
+//3: Lưu trữ sự thay đổi
+// git add "tên file"
+// git add . <lưu tất cả sự thay đổi>
+
+//4: Commit vào repository
+// git commit -m "lưu file buổi 1"
+
+//git config --global user.name "HoVanTrong"
+//git config --global user.email "vantrong169.ute@gmail.com"
+
+// Các bước hình thành một new folder
+// 1. git init
+// 2. tạo file mới
+// 3. git add . (lưu file mới)
+// 4. git commit -m "message" (đóng gói lưu trữ cho phiên làm việc)
+
+
+//ctrl + ~ :mở terminal nhanh
+//1 - khai bao bien (dynamic type)
+//var a = 5
+//let b = 10
+//const c =15
+//c = 10
+//console.log(c)
