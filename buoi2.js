a573cac7b0b93aad0ff18818e4f7c812c5795070
HoVanTrong <vantrong169.ute@gmail.com>
2021-04-27 19:05:01 +0700

    higher order func

--- buoi2.js
+++ buoi2.js
@@ -64,17 +64,43 @@
 //      }
 //  }
 // bai 12 - array method
-const arrNumber = [1,2,5,4,7,5]
-// // them phan tu cuoi mang
+// const arrNumber = [1,2,5,4,7,5]
+// // // // them phan tu cuoi mang
 // arrNumber.push(7)
-// // xoa phan tu cuoi
+// // // // xoa phan tu cuoi
 // arrNumber.pop()
-// // them phan tu o dau
+// // // // them phan tu o dau
 // arrNumber.unshift(15)
-// //xoa phan tu o dau
-arrNumber.shift()
-//xoa phan tu o giua
+// // // //xoa phan tu o dau
+// arrNumber.shift()
+// // //xoa phan tu o giua
 // arrNumber.splice(1,4)
-// them o giua
+// // // them o giua
 // arrNumber.splice(1,0,20,21,22,23)
-console.log(arrNumber)
+// console.log(arrNumber)
+
+// bai tap 1......
+// function removeEnd(arr,n){
+//     arr.splice(arr.length - n,n)
+//     return arr
+// }
+// console.log(removeEnd([2,3,1,8,9,7],3))
+
+// bai tap 2......
+// function first(arr, n) {
+//     return arr.slice(0,n)
+// }
+// console.log(first([1, 2, 3], 2)); // expect [1, 2]
+
+// bai 13 - higher order function
+function nhan2(number){
+    return number * 2
+}
+function tinhTong(arr,func){
+    let result = 0
+    for (const value of arr){
+        result += value
+    }
+    return func(result)
+}
+console.log(tinhTong([1,2],nhan2))
