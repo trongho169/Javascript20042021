41c3902c7e5f1119efa1cc70ed974e630ddc3596
HoVanTrong <vantrong169.ute@gmail.com>
2021-04-27 18:04:21 +0700

    ham for in

--- buoi2.js
+++ buoi2.js
@@ -1,6 +1,34 @@
 //bai 10 - for of chi su dung cho array
-const arrNames = ["ti","suu","dan"]
+// const arrNames = ["ti","suu","dan"]
+// const ti = {
+//     ten: "nguyenvanti",
+//     tuoi: 25
+// }
+// for (const value of arrNames){
+//     console.log(value)
+// }
 
-for (const value of arrNames){
-    console.log(value)
+// su dung vong lap for...of de tinh tich cua cac phan tu trong mot array
+// function multilply(arr){
+//     if ( Array.isArray(arr)){
+//         let result = 1
+//         for (const value of arr){
+//             result *= value
+//         }
+//         return result
+//     }
+//     else{
+//         return "gia tri khong phai la mang"
+//     }
+// }
+// console.log(multilply([2,3,4]));
+
+// bai 11 - for in
+const arrNames = ["ti","suu","dan"]
+const ti = {
+    name: "nguyenvanti",
+    age: 25
+}
+for(const key in ti){
+    console.log(key)
 }
