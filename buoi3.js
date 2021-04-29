eaa3697409bb0cbc561a41468ea738b4d6297021
HoVanTrong <vantrong169.ute@gmail.com>
2021-04-29 18:07:42 +0700

    ham filter

--- buoi3.js
+++ buoi3.js
@@ -1,1 +1,9 @@
 // bai 15 - ham filter
+const arrNames = ["tèo","tí","hoa","tuấn","dương"]
+const newArrNames = arrNames.filter(function(name){
+    if (name.length <= 3){
+        return true;
+    }
+    return false
+})
+console.log(newArrNames)
